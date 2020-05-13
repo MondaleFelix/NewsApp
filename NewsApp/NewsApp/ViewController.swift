97771c5ed86e1c22f98d33b8915be991c688353b
MondaleFelix <mondalefelix@yahoo.com>
2020-05-12 23:01:08 -0700

    initial commit

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Mondale on 5/12/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
